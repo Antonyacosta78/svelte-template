4b2f397b85795e9a93e2eabe288395440515e80e
Antony Acosta <aacosta@switch.com.uy>
2019-11-17 11:52:48 -0300

    updated gitignore, renamed public to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,27 @@
 <script>
-	export let name = "Antony";
+	let name = "Antony";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello {name} COMO TAS</h1>
 </main>
 
-<style>
+<style type="text/scss">
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+    h1 {
+		  color: #ff3e00;
+		  text-transform: uppercase;
+		  font-size: 4em;
+		  font-weight: 100;
+	  }
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+
+
 
 	@media (min-width: 640px) {
 		main {
